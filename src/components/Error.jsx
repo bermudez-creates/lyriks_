8b3df627d3036df350144df4a0297e667bd27074
Steezy <bermudezalejandro45@gmail.com>
2024-02-09 18:25:17 -0500

    API does not have images avail.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="w-full justify-center flex items-center ">
+    <h1 className="font-bold text-white text-2x1 mt-2">
+      Something went wrong...
+    </h1>
+  </div>
 );
 
 export default Error;
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,31 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import PlayPause from './PlayPause';
+
+const SongCard = ({ song, index }) => {
+  const activeSong = 'Test';
+  console.log('song card --', song);
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/80 bg-opacity-50 backdrop-blur-sm animate-slideup rounded-lg curser-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong?.title === song.title
+              ? `flex bg-black-opacity-70`
+              : 'hidden'
+          }`}
+        >
+          <PlayPause />
+        </div>
+        <img
+          alt={`Song image for: ${song?.title}`}
+          src={song.photo_url?.photo_url}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SongCard;
